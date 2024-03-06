758b7bc0f640798ed7650dc9d8443617d94e6d61
Dhruv <gohildhruvesh111@gmail.com>
2024-03-06 20:08:29 +0530

    directive_basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import test from 'node:test';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule , CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my_angular_app';
+  name = 'Dhruv';
+  mobile = 8469511356;
+  age = 24;
+  inputvalue = 'test';
+
+  onchange(e: Event) {
+    const value = (e.target as HTMLInputElement).value;
+    this.inputvalue = value;
+  }
+
+  user = [
+    { name: 'aarav', age: 18, course: 'B.tech' },
+    { name: 'dhruv', age: 24, course: 'M.sc' },
+    { name: 'ronak', age: 19, course: 'MBBS' },
+  ];
 }
